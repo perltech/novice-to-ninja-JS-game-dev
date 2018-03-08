4b72b433df52f8d3bd116b749a01e94be004b549
perltech <noah.perlmutter@gmail.com>
2018-03-08 10:34:51 -0700

    get canvas context

--- chapter2/canvasAPI/src/main.js
+++ chapter2/canvasAPI/src/main.js
@@ -1,17 +1,3 @@
-// const gameType = "awesome";
-// alert(`Let's make ${ gameType } games!`);
-
-const myGuess = Math.floor(Math.random() * 20) + 1;
-let guesses = 0;
-let guess;
-
-while (guess !== myGuess) {
-    guess = parseInt(prompt("What number am I thinking of?"), 10);
-    guesses++;
-    if (guess < myGuess) {
-        alert("Higher.");
-    } else if (guess > myGuess) {
-        alert("Lower.");
-    }
-}
-alert(`Well done! You got it in ${ guesses }!`);
+const canvas = document.querySelector('#board canvas');
+const ctx = canvas.getContext("2d");
+console.log(ctx.canvas);
