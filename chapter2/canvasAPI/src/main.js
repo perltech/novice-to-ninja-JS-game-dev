5825d25a9802c39152977f775aab5f0ca0694585
perltech <noah.perlmutter@gmail.com>
2018-03-08 12:58:46 -0700

    add star field

--- chapter2/canvasAPI/src/main.js
+++ chapter2/canvasAPI/src/main.js
@@ -1,3 +1,23 @@
 const canvas = document.querySelector('#board canvas');
 const ctx = canvas.getContext("2d");
 console.log(ctx.canvas);
+
+
+// Set color styles
+ctx.strokeStyle = "black";
+ctx.fillStyle = "red";
+
+// Draw square
+ctx.fillRect(300, 200, 50, 50);
+ctx.strokeRect(300, 200, 50, 50);
+
+// Draw circle
+ctx.beginPath();
+ctx.arc(325, 170, 25, 0, Math.PI * 2, false);
+ctx.fill();
+ctx.stroke();
+
+// Convert degrees and radians
+const deg2Rad = deg => (Math.PI / 180) * deg;
+const rad2Deg = rad => (180 / Math.PI) * rad;
+
