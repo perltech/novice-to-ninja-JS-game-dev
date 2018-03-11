4fd839d91eeb3a4a34b3fea99f10da73e64fd0f1
perltech <noah.perlmutter@gmail.com>
2018-03-11 11:27:04 -0600

    add set interval and set timeout examples

--- chapter3/infinite-game-loops/src/main.js
+++ chapter3/infinite-game-loops/src/main.js
@@ -1,7 +1,15 @@
+const canvas = document.querySelector("#board canvas");
+const ctx = canvas.getContext("2d");
+const {
+    width: w,
+    height: h
+} = canvas;
+
 const start = Date.now();
 // Set interval for 60 frames per second
 const timer = setInterval(() => {
     // Clear the screen
+    ctx.fillStyle="#F0F0F0";
     ctx.fillRect(0, 0, w, h);
     // Write the time
     ctx.strokeText(Date.now() - start, 20, 80);
@@ -10,3 +18,19 @@ const timer = setInterval(() => {
         clearInterval(timer);
     }
 }, 1000 / 60);
+
+/* Using set timeout */
+// set timeout only loops once, needs to be recalled recursively
+
+// const start = Date.now();
+// function loopy () {
+// ctx.fillRect(0, 0, w, h);
+// ctx.strokeText(Date.now() - start, 20, 80);
+// if (Math.random() < 0.01) {
+// ctx.strokeText("Game Over!", 160, 180);
+// } else {
+// // Loop
+// setTimeout(loopy, 1000 / 60);
+// }
+// }
+// loopy(); // Start things running!
