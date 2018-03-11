92b9521d9208cfedc7a83657cfb0ba1e6ba634f1
perltech <noah.perlmutter@gmail.com>
2018-03-11 11:16:24 -0600

    add blending example and logo

---
+++ chapter2/blending-and-alpha/src/main.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector("#board canvas");
+const ctx = canvas.getContext("2d");
+const {
+    width: w,
+    height: h
+} = canvas;
+
+
+function draw() {
+    for (let i = 0; i < 100; i++) {
+        const x = Math.random() * w;
+        const y = Math.random() * h;
+        ctx.beginPath();
+        ctx.arc(x, y, Math.random() * 50, 0, Math.PI * 2, true);
+        ctx.fill();
+    }
+}
+
+// Save as opaque
+ctx.save();
+// Set transparency
+ctx.globalAlpha = 0.3;
+ctx.fillStyle = "blue";
+draw();
+ctx.fillStyle = "orange";
+draw();
+ctx.fillStyle = "green";
+draw();
+// Restores opaqueness
+ctx.restore();
+ctx.fillStyle = "lemonchiffon";
+draw();
