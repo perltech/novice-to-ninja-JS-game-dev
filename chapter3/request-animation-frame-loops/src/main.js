f1ae3d9277c359101adaba9327fc75a1b6b5b5f4
perltech <noah.perlmutter@gmail.com>
2018-03-11 11:43:06 -0600

    add set-interval loop example and request animation frame example

---
+++ chapter3/request-animation-frame-loops/src/main.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector("#board canvas");
+const ctx = canvas.getContext("2d");
+const {
+    width: w,
+    height: h
+} = canvas;
+
+
+// Game setup code
+// Set background color
+ctx.fillStyle = "#000";
+ctx.globalAlpha = 0.02;
+
+function loopy(t) {
+    requestAnimationFrame(loopy);
+    // Game logic code
+    // Set drop color
+    ctx.save();
+    ctx.fillRect(0, 0, w, h);
+    ctx.fillStyle = "#fff";
+    ctx.globalAlpha = 1;
+
+    // Random circle
+    const x = Math.random() * w;
+    const y = Math.random() * h;
+    const radius = Math.random() * 20;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.restore();
+
+}
+requestAnimationFrame(loopy); // Start things running!
