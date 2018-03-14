ec42059c29417ef51b4ae7095173e7e0357d3d81
perltech <noah.perlmutter@gmail.com>
2018-03-14 08:26:35 -0600

    add mouse input example

--- chapter3/mouse-input/lib/MouseControls.js
+++ chapter3/mouse-input/lib/MouseControls.js
@@ -1,52 +1,45 @@
 class MouseControls {
     constructor(container) {
-        this.el = container || document.body;
-        // State
-        this.pos = {
-            x: 0,
-            y: 0
-        };
-        this.isDown = false;
-        this.pressed = false;
-        this.released = false;
-
-
-        // Handlers
-        document.addEventListener("mousemove", this.move.bind(this), false);
-        document.addEventListener("mousedown", this.down.bind(this), false);
-        document.addEventListener("mouseup", this.up.bind(this), false);
-
-        function mousePosFromEvent({clientX,clientY}) {
-            const {el, pos} = this;
-            // Get size of canvas by subtracting from DOM
-            const rect = el.getBoundingClientRect();
-            const xr = el.width / el.clientWidth;
-            const yr = el.height / el.clientHeight;
-            //
-            pos.x = (clientX - rect.left) * xr;
-            pos.y = (clientY - rect.top) * yr;
-        }
-
-        function move(e) {
-            this.mousePosFromEvent(e);
-        }
-
-        function down(e) {
-            this.isDown = true;
-            this.pressed = true;
-            this.mousePosFromEvent(e);
-        }
-
-        function up() {
-            this.isDown = false;
-            this.released = true;
-        }
-
-        // To check state every frame for single mouse click presses, as to not confuse that with a held mouse press
-        function update() {
-            this.released = false;
-            this.pressed = false;
-        }
+      this.el = container || document.body;
+  
+      this.pos = { x: 0, y: 0 };
+      this.isDown = false;
+      this.pressed = false;
+      this.released = false;
+  
+      // Handlers
+      document.addEventListener("mousedown", e => this.down(e), false);
+      document.addEventListener("mouseup", e => this.up(e), false);
+      document.addEventListener("mousemove", e => this.move(e), false);
     }
-}
+  
+    mousePosFromEvent({ clientX, clientY }) {
+      const { el, pos } = this;
+      const rect = el.getBoundingClientRect();
+      const xr = el.width / el.clientWidth;
+      const yr = el.height / el.clientHeight;
+      pos.x = (clientX - rect.left) * xr;
+      pos.y = (clientY - rect.top) * yr;
+    }
+  
+    down(e) {
+      this.isDown = true;
+      this.pressed = true;
+      this.mousePosFromEvent(e);
+    }
+  
+    up() {
+      this.isDown = false;
+      this.released = true;
+    }
+  
+    move(e) {
+      this.mousePosFromEvent(e);
+    }
+  
+    update() {
+      this.released = false;
+      this.pressed = false;
+    }
+  }
 export default MouseControls;
--- chapter3/mouse-input/src/main.js
+++ chapter3/mouse-input/src/main.js
@@ -1,38 +1,29 @@
-import KeyControls from "../lib/KeyControls.js";
+import MouseControls from '../lib/MouseControls.js';
 
 const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
-const {
-    width: w,
-    height: h
-} = canvas;
+const mouse = new MouseControls(canvas);
 
 // Game setup code
-let dt = 0;
-let last = 0;
-
-let x = w / 2;
-let y = h / 2;
 let color = 0;
-const controls = new KeyControls();
 
-function loopy(ms) {
-    requestAnimationFrame(loopy);
-    const t = ms / 1000; // Let's work in seconds
-    dt = t - last;
-    last = t;
+function loopy() {
+  requestAnimationFrame(loopy);
 
-    // Game logic code
-    x += controls.x;
-    y += controls.y;
-    if (!controls.action) {
-        color += 10;
-        if (color > 360) {
-            color -= 360;
-        }
+  // Game logic code
+  const x = mouse.pos.x;
+  const y = mouse.pos.y;
+  if (mouse.pressed) {
+    color += 10;
+    if (color > 360) {
+      color -= 360;
     }
-    // Draw the rectangle
-    ctx.fillStyle = `hsl(${color}, 50%, 50%)`;
-    ctx.fillRect(x, y, 50, 50);
+  }
+
+  // Draw the rectangle
+  ctx.fillStyle = `hsl(${color}, 50%, 50%)`;
+  ctx.fillRect(x - 25, y - 25, 50, 50);
+
+  mouse.update();
 }
 requestAnimationFrame(loopy);
