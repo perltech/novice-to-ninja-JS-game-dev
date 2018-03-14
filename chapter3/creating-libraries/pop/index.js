a91addd0ca0b09e5ca3de4a5d79b149d72598fb2
perltech <noah.perlmutter@gmail.com>
2018-03-14 14:25:44 -0600

    create and add container

---
+++ chapter3/creating-libraries/pop/index.js
@@ -0,0 +1,10 @@
+import Container from './Container.js';
+import KeyControls from "./controls/KeyControls.js";
+import MouseControls from "./controls/MouseControls.js";
+
+
+export default {
+    Container,
+    KeyControls,
+    MouseControls   
+};
