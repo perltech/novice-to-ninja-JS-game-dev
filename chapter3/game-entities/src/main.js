91990bde2c0c604b050421278de2182b95c4dc06
perltech <noah.perlmutter@gmail.com>
2018-03-15 01:11:56 -0600

    add animation to ships

--- chapter3/game-entities/src/main.js
+++ chapter3/game-entities/src/main.js
@@ -16,11 +16,24 @@ document.querySelector("#board").appendChild(renderer.view);
 // Game objects
 const scene = new Container();
 const texture = new Texture("res/images/spaceship.png");
+
 for (let i = 0; i < 50; i++) {
+  const speed = Math.random() * 150 + 50;
   const ship = new Sprite(texture);
-  ship.pos.x = Math.random() * w;
-  ship.pos.y = Math.random() * h;
+  const {
+    pos
+  } = ship;
+
+  pos.x = Math.random() * w;
+  pos.y = Math.random() * h;
   scene.add(ship);
+
+  ship.update = function (dt) {
+    this.pos.x += speed * dt;
+    if (this.pos.x > w) {
+      this.pos.x = -32;
+    }
+  };
 }
 
 let dt = 0;
