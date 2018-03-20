ebca4b2921f8ae4acede26ce055633fa146cff7c
perltech <noah.perlmutter@gmail.com>
2018-03-20 12:38:09 -0600

    add background and ship to shooter

--- chapter3/space-shooter/src/main.js
+++ chapter3/space-shooter/src/main.js
@@ -2,6 +2,7 @@ import pop from "../pop/index.js";
 const {
   Container,
   CanvasRenderer,
+  KeyControls,
   Texture,
   Sprite
 } = pop;
@@ -9,32 +10,39 @@ const {
 
 // Game setup code
 const w = 640;
-const h = 480;
+const h = 300;
 const renderer = new CanvasRenderer(w, h);
 document.querySelector("#board").appendChild(renderer.view);
 
 // Game objects
 const scene = new Container();
-const texture = new Texture("res/images/spaceship.png");
 
-for (let i = 0; i < 50; i++) {
-  const speed = Math.random() * 150 + 50;
-  const ship = new Sprite(texture);
-  const {
-    pos
-  } = ship;
+// Load game textures
+const textures = {
+  background: new Texture("res/images/bg.png"),
+  spaceship: new Texture("res/images/spaceship.png")
+};
 
-  pos.x = Math.random() * w;
-  pos.y = Math.random() * h;
-  scene.add(ship);
+const controls = new KeyControls();
+// Make a spaceship
+const ship = new Sprite(textures.spaceship);
+ship.pos.x = 120;
+ship.pos.y = h / 2 - 16;
+ship.update = function (dt, t) {
+  // Update the player position
+  const {pos} = this;
+  pos.x += controls.x * dt * 200;
+  pos.y += controls.y * dt * 200;
 
-  ship.update = function (dt) {
-    this.pos.x += speed * dt;
-    if (this.pos.x > w) {
-      this.pos.x = -32;
-    }
-  };
-}
+  if (pos.x < 0) pos.x = 0;
+  if (pos.x > w) pos.x = w;
+  if (pos.y < 0) pos.y = 0;
+  if (pos.y > h) pos.y = h;
+};
+
+// Add everything to the scene container
+scene.add(new Sprite(textures.background));
+scene.add(ship);
 
 let dt = 0;
 let last = 0;
