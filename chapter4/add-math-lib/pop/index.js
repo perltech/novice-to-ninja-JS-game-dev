77d7b31a735e4eb71df9d7a972f0eea5df04063a
perltech <noah.perlmutter@gmail.com>
2018-03-23 10:49:32 -0600

    initialize math utility

---
+++ chapter4/add-math-lib/pop/index.js
@@ -0,0 +1,24 @@
+import Game from './Game.js';
+import Container from './Container.js';
+import KeyControls from "./controls/KeyControls.js";
+import MouseControls from "./controls/MouseControls.js";
+
+import CanvasRenderer from "./renderer/CanvasRenderer.js";
+
+import Text from "./Text.js";
+import Texture from "./Texture.js";
+import Sprite from "./Sprite.js";
+
+import math from './utils/math.js';
+
+
+export default {
+    Game,
+    Container,
+    KeyControls,
+    MouseControls,
+    CanvasRenderer,
+    Text,
+    Texture,
+    Sprite
+};
